d7bf64744add6abe8c95f1ded1860596be3cd6c7
henry753951 <henryboy753951@gmail.com>
2025-01-19 20:48:47 +0800

    feat(ui): 新增多個表單元件

---
+++ components/global/modals/Patent/CreateModal.vue
@@ -0,0 +1,35 @@
+<template>
+   <div>
+      <Sheet v-model:open="isOpen">
+         <SheetContent :side="props.side">
+            <SheetHeader>
+               <SheetTitle>新增專利</SheetTitle>
+               <SheetDescription>
+                  請填寫專利資訊
+               </SheetDescription>
+            </SheetHeader>
+         </SheetContent>
+      </Sheet>
+   </div>
+</template>
+
+<script lang="ts" setup>
+import {
+   Sheet,
+   SheetContent,
+   SheetDescription,
+   SheetHeader,
+   SheetTitle,
+} from "@/components/ui/sheet";
+
+const isOpen = defineModel("open", {
+   type: Boolean,
+   default: false,
+});
+
+const { props } = defineProps<{
+   props: Record<string, any>
+}>();
+</script>
+
+<style scoped></style>
