261e1b4b876bc4b92c33b2f56ef889e18daa9184
henry753951 <henryboy753951@gmail.com>
2025-03-14 19:03:02 +0800

    feat(patent): 新增基本專利編輯區塊元件，包含數據綁定與監聽邏輯

---
+++ components/Block/Patent/View/EditBlock/Basic.vue
@@ -0,0 +1,22 @@
+<template>
+   <div></div>
+</template>
+
+<script lang="ts" setup>
+const patent = defineModel({
+   type: Object as PropType<RouterOutput["data"]["patent"]["getPatent"]>,
+   required: true,
+});
+
+const cacheData = ref<RouterOutput["data"]["patent"]["getPatent"] | null>(null);
+
+watch(
+   () => patent.value,
+   (value) => {
+      cacheData.value = value;
+   },
+   { immediate: true },
+);
+</script>
+
+<style scoped></style>
