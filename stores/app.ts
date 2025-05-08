5383a0c57251b8b2dd29830db8bfe470b6ec7d72
binhan <qweqazhank123@gmail.com>
2025-05-08 11:33:40 +0800

    fix: 移除不必要的導入，更新構建腳本以包含 Prisma 生成

--- stores/app.ts
+++ stores/app.ts
@@ -1,6 +1,5 @@
 import { defineStore } from "pinia";
 import { toast } from "vue-sonner";
-import type { dbZ, z } from "#imports";
 export const useAppStore = defineStore("appStore", {
    state: () => {
       return {
